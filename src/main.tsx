0a83b3a1c6a74366b68082ab9421eaed003b6767
Katy Morris <kathy@sonatainnovations.com>
2025-11-30 16:52:47 -0700

    push

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/km-website">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
